04b96fc02655bd17a2af2b26f8e712aa88c4cac4
yudafhd <yuda@192.168.1.7>
2025-11-08 22:03:28 +0700

    style: add pwa browser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   if (!accessToken) {
     return (
-      <div className="min-h-screen bg-gradient-to-b from-[#0e1525] to-[#0a0f1e] font-sans text-zinc-100 flex items-center justify-center px-4 sm:px-8">
+      <div className="min-h-[100svh] bg-gradient-to-b from-[#0e1525] to-[#0a0f1e] font-sans text-zinc-100 flex items-center justify-center px-4 sm:px-8">
         <main className="w-full max-w-6xl flex flex-col items-center gap-2 md:gap-10 py-16 sm:py-24 text-center">
           <div className="relative inline-block">
             <h1 className="text-[42px] sm:text-[64px] md:text-[88px] lg:text-[120px] font-extrabold leading-none tracking-tight text-zinc-200">
@@ -37,7 +37,7 @@ export default async function Home() {
   const bookmarks = await readBookmarks(accessToken);
 
   return (
-    <div className="min-h-screen min-w-screen bg-gradient-to-b from-[#0e1525] to-[#0a0f1e] text-zinc-100">
+    <div className="min-h-[100svh] min-w-screen bg-gradient-to-b from-[#0e1525] to-[#0a0f1e] text-zinc-100">
       <BookmarksView bookmarks={bookmarks} />
     </div>
   );
